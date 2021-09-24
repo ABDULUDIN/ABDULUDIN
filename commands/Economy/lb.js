87ffce14648b6b444b6546c885a74e305bc43103
ABDULUDIN <91243050+ABDULUDIN@users.noreply.github.com>
2021-09-24 13:24:42 +0700

    1

--- commands/Economy/lb.js
+++ commands/Economy/lb.js
@@ -4,9 +4,12 @@ module.exports = ({
     description: "Check the money leaderboard",
     usage: "",
     category: "economy",
-    code: `$title[Leaderboard Uang]
+    code: `
+    $If[$message==uang]
+    $title[Leaderboard Uang]
     $color[RANDOM]
     $description[$globalUserLeaderBoard[Money;asc; {top}. **{username}**- 💵{value}]]
     $cooldown[5s;{description: Cooldown **time%**!}{color:RANDOM}]
-    $onlyIf[$checkContains[$channelType;text;news]==true;]`
+    $onlyIf[$checkContains[$channelType;text;news]==true;
+    $endIf]`
 })
