9334645151ef44af72c6ef48f3db8a0ace932bcb
ABDULUDIN <91243050+ABDULUDIN@users.noreply.github.com>
2021-09-24 13:37:31 +0700

    Create Shop.js

---
+++ commands/Economy/Shop.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: "shop",
+  code: `
+  $title[Shop]
+  $description[
+
+MALL XZR
+HP 💵 50JT
+LAPTOP 💵 100JT
+KUOTA 
+PULSA
+PASANG WIFI (XZR NETWORK) 💵 500K
+BELI BAJU (MERK XZR) 💵 100K
+BELI CELANA (MERK XZR) 💵 100K]
+$color[RANDOM]`
+}
